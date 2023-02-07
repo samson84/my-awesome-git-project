4c9bed8c48609e18f706a265518b408a827f6bc6
Daniel Hatas <daniel.hatas@codecool.com>
2023-02-07 11:40:16 +0100

    beers can be sorted by name

--- sortBy.js
+++ sortBy.js
@@ -2,7 +2,10 @@ const getBeers = require('./data')
 
 function main(property) {
     console.log('property:', property)
-
+    const beers = getBeers()
+    const sorted = [...beers]
+    sorted.sort((left, right) => left.abv - right.abv)
+    beers.forEach(beer=> console.log(`${beer.name}, ${beer.abv}`))
 }
 
 main(process.argv[2])
